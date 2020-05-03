4d6b3ab9eb2e8dc0c7642675d2491cc76381f8f3
Danilo Collares <dandee@localhost.localdomain>
2020-05-03 00:40:52 -0300

    Base page structure for library search view

--- Mobile/src/app/tab1/tab1.page.ts
+++ Mobile/src/app/tab1/tab1.page.ts
@@ -7,14 +7,24 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  items:any[] = [
+  // Temporary variable that will go away one day I hope
+  libraries:any[] = [
     {name:"Babel",book_amount:77},
     {name:"Tipo Babel só que não",book_amount:95},
     {name:"Big boy",book_amount:1000},
     {name:"Nome gigante que provavelmente vai quebrar o role todo fodase quero testar aaaa",book_amount:45},
-    // {name:"Babel",book_amount:77},
-    // {name:"Babel",book_amount:77},
-    // {name:"Babel",book_amount:77},
+    {name:"Babel",book_amount:77},
+    {name:"Tipo Babel só que não",book_amount:95},
+    {name:"Big boy",book_amount:1000},
+    {name:"Nome gigante que provavelmente vai quebrar o role todo fodase quero testar aaaa",book_amount:45},
+    {name:"Babel",book_amount:77},
+    {name:"Tipo Babel só que não",book_amount:95},
+    {name:"Big boy",book_amount:1000},
+    {name:"Nome gigante que provavelmente vai quebrar o role todo fodase quero testar aaaa",book_amount:45},
+    {name:"Babel",book_amount:77},
+    {name:"Tipo Babel só que não",book_amount:95},
+    {name:"Big boy",book_amount:1000},
+    {name:"Nome gigante que provavelmente vai quebrar o role todo fodase quero testar aaaa",book_amount:45},
   ];
   constructor() {}
 
